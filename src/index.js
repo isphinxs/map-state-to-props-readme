ca6a8bcc51c40475de196d22180acac966f08e7e
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-14 20:56:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import counterReducer from './reducers/counterReducer.js';
+import { Provider } from 'react-redux'; 
+// import counterReducer from './reducers/counterReducer.js';
+import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
 
 const store = createStore(
-    counterReducer,
+    // counterReducer,
+    shoppingListItemReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ); 
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
